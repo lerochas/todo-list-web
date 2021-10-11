9c60c7b8a6f257b037476b72d895fbcb1685fa89
lerochas <emaildole.07@gmail.com>
2021-10-11 17:30:02 -0300

    so now we have global styles

---
+++ src/global/GlobalStyle.js
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none !important;
+  }
+  html, body, #root {
+    min-height: 100%;
+  }
+  body {
+    font-family: Lato,system,-apple-system,sans-serif;
+    background-color: #fff !important;
+  }
+  a {
+    text-decoration: none;
+  }
+  main {
+    background-image: linear-gradient(to bottom right, #eafaf4, #fff);
+    padding-bottom: 20px;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.24);
+  }
+  
+  .css-hip9hq-MuiPaper-root-MuiAppBar-root {
+    background-color: #fff !important;
+  }
+
+  .css-i4bv87-MuiSvgIcon-root {
+    color: #00e6b8 !important;
+  }
+`;
+
+export default GlobalStyle;
